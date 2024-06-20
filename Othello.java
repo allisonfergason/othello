6b4ff58e4954d5f968c601c3d5cb27e24f587867
Allison Fergason <154763261+allisonfergason@users.noreply.github.com>
2024-06-20 18:05:32 -0400

    Update Othello.java

--- Othello.java
+++ Othello.java
@@ -194,6 +194,9 @@ public class Othello {
         }
         else if (onepoints < twopoints) {
             System.out.print(player2+" won! ");
+            if (player2.equals("Baba Burtis")) {
+                System.out.println("-500 aura for "+player1);
+            }
         }
         else {
             System.out.print("There was a tie! ");
@@ -436,8 +439,32 @@ public class Othello {
         return false;
     } 
 
-    // print rules moethod
+    // print rules method
     public static void printRules() {
-        System.out.println("rules");
+        System.out.println();
+        System.out.println();
+        System.out.println("Rules sourced from World Othello Federation.");
+        System.out.println("Each player takes 32 discs and chooses one colour to use throughout the game. Black places two black discs");
+        System.out.println("and White places two white discs as shown below.");
+        System.out.println();
+        drawBoard(gameBoard);
+        System.out.println();
+        System.out.println("A move consists of 'outflanking' your opponent's disc(s), then flipping the outflanked disc(s)to your colour.");
+        System.out.println("To outflank means to place a disc on the board so that your opponent's row (or rows) of disc(s) is bordered at each");
+        System.out.println("end by a disc of your colour. (A 'row' may be made up of one or more discs).");
+
+        System.out.println();
+        System.out.println("Rules");
+        System.out.println();
+        System.out.println("1. Black always moves first.");
+        System.out.println("2. If on your turn you cannot outflank and flip at least one opposing disc, your turn is forfeited and your opponent moves\n" + //
+                "again. However, if a move is available to you, you may not forfeit your turn. ");
+        System.out.println("4. Players may not skip over their own colour disc(s) to outflank an opposing disc.");
+        System.out.println("5. Disc(s) may only be outflanked as a direct result of a move and must fall in the direct line of the disc placed down.");
+        System.out.println("6. All discs outflanked in any one move must be flipped, even if it is to the player's advantage not to flip them at all. ");
+        System.out.println("7. Once a disc is placed on a square, it can never be moved to another square later in the game. ");
+        System.out.println("8. When it is no longer possible for either player to move, the game is over. Discs are counted and the player with the majority of their colour showing is the winner.");
+        System.out.println("Note: It is possible for a game to end before all 64 squares are filled.");
+
     }
 }
